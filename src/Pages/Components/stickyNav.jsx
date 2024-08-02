0ed528c11c622844056e7bf4b7c775200230b1f7
AnDev02 <135408714+AnDev02@users.noreply.github.com>
2024-08-03 03:19:29 +0700

    update sticky nav

--- src/Pages/Components/stickyNav.jsx
+++ src/Pages/Components/stickyNav.jsx
@@ -345,17 +345,6 @@ export default function StickyNav() {
                                     </Box>
                                 </div> : ""}
                             </div>
-                            <Box className={`searchbox ${(toggleSearch === true) ? 'searchbox-active' : ''} ${(toggleSearch === true) ? 'searchbar-active' : ''}`} sx={{
-                                marginTop: '5px',
-                                color: opac ? "white" : "white"
-                            }}>
-                                {(toggleSearch === true) ? <Overlay func={handleToggleSearch} /> : ""}
-                                <div className='right-nav search-ic' onClick={handleToggleSearch}><SearchIcon style={{ fontSize: '10px !important', color: "white !important" }} /></div>
-                                <div className="gr-search">
-                                    <input className={`searchbar`} type="text" value={searchValue} onChange={onSearch} />
-                                    <button className="btn-search" onClick={handleSearch}><SearchIcon style={{ fontSize: '10px !important' }} /></button>
-                                </div>
-                            </Box>
                         </div>
                     </Toolbar>
                 </AppBar>
